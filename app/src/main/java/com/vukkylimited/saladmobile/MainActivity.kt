e478144eb3144cb04172d2938847d53cecb8996c
Vukky <46850780+VukAnd@users.noreply.github.com>
2020-09-15 19:49:49 +0200

    Add portrait support

--- app/src/main/java/com/vukkylimited/saladmobile/MainActivity.kt
+++ app/src/main/java/com/vukkylimited/saladmobile/MainActivity.kt
@@ -6,9 +6,6 @@ import android.app.AlertDialog
 import android.content.pm.ActivityInfo
 import android.content.res.Configuration
 import android.os.Bundle
-import android.util.DisplayMetrics
-import android.util.Log
-import android.view.ViewGroup
 import android.webkit.WebView
 
 
@@ -28,30 +25,14 @@ class MainActivity : Activity() {
         saladBrowsing.settings.loadWithOverviewMode = true
 
         // Device checks
-        val display = windowManager.defaultDisplay
-        val outMetrics = DisplayMetrics()
-        display.getMetrics(outMetrics)
-
-        val density = resources.displayMetrics.density
-        val dpWidth = outMetrics.widthPixels / density
-        if (dpWidth < 768) {
-            AlertDialog.Builder(this)
-                .setTitle(getString(R.string.issues_title))
-                .setMessage(getString(R.string.width_issues))
-                .setPositiveButton(android.R.string.ok, null)
-                .setIcon(android.R.drawable.ic_dialog_info)
-                .show()
-        } else if (dpWidth > 768) {
-            saladBrowsing.layoutParams.width = ViewGroup.LayoutParams.MATCH_PARENT
-        }
 
         val orientation = resources.configuration.orientation
-        if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
             AlertDialog.Builder(this)
                 .setTitle(getString(R.string.portrait_title))
                 .setMessage(getString(R.string.portrait_message))
                 .setPositiveButton(android.R.string.yes) { _, _ ->
-                    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE
+                    requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
                 }
                 .setNegativeButton(android.R.string.no, null)
                 .setIcon(android.R.drawable.ic_dialog_alert)
